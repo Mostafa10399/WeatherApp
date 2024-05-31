ed5003ef56b1933fd8dc12ae9d02c7ddeca1b026
Mostafa10399 <mostafaelshall972@gmail.com>
2024-05-31 15:11:06 +0300

    first commit

---
+++ WeatherAppTask/WeatherAppTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTaskApp.swift
+//  WeatherAppTask
+//
+//  Created by Mostafa Mahmoud on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
